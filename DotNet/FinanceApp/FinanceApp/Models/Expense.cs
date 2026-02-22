849fa8c3067c3d859c8402f53da616a0bfedf09e
denidim <fs900220@gmail.com>
2026-02-22 21:22:25 +0000

    Add Expence class

---
+++ DotNet/FinanceApp/FinanceApp/Models/Expense.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinanceApp.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage ="Amount needs to be higher than 0")]
+        public double Amount { get; set; }
+
+        [Required]
+        public string? Category { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}

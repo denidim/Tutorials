55a29c7c08bf45e663ad6ed1c6854e00ecbdba62
denidim <fs900220@gmail.com>
2026-02-22 23:16:09 +0000

    changes

--- DotNet/FinanceApp/FinanceApp/Data/FinanceAppContext.cs
+++ DotNet/FinanceApp/FinanceApp/Data/FinanceAppContext.cs
@@ -5,7 +5,10 @@ namespace FinanceApp.Data
 {
     public class FinanceAppContext : DbContext
     {
-        public FinanceAppContext(DbContextOptions<FinanceAppContext> options):base(options) { }
+        public FinanceAppContext(DbContextOptions<FinanceAppContext> options)
+            :base(options) 
+        { 
+        }
 
         DbSet<Expense> Expenses { get; set; }
 

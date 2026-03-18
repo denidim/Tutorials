aad856ffa2ccd2d5b892a671a9d3d98b546f7fd7
denidim <fs900220@gmail.com>
2026-03-18 20:47:52 +0000

    Add class Pizza

---
+++ DotNet/ExtensionMethodsExample/ExtensionMethodsExample/Pizza.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodsExample
+{
+    internal class Pizza
+    {
+        public List<string> Toppings { get; set; } = new List<string>();
+        public void ShowToppings() => Console.WriteLine($"Your Pizza has: {string.Join(", ", Toppings)}");
+    }
+}

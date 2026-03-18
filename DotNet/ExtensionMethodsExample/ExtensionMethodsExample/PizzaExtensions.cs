05f72be4a0ffda104d44945cebeaf4e5cdf7109d
denidim <fs900220@gmail.com>
2026-03-18 20:48:34 +0000

    Add static PizzaExtension class + method

---
+++ DotNet/ExtensionMethodsExample/ExtensionMethodsExample/PizzaExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodsExample
+{
+    internal static class PizzaExtensions
+    {
+        public static Pizza AddTopping<T>(this Pizza pizza) where T : new()
+        {
+            var topping = new T();
+
+            string toppingName = topping.GetType().Name;
+
+            pizza.Toppings.Add(toppingName);
+
+            Console.WriteLine($"Added {toppingName} to the pizza!");
+
+            return pizza;
+        }
+    }
+}

307193be361cd14bd6194eafc1fe8a4a0d9063b7
denidim <fs900220@gmail.com>
2026-03-18 20:49:11 +0000

    Add logic

---
+++ DotNet/ExtensionMethodsExample/ExtensionMethodsExample/Program.cs
@@ -0,0 +1,11 @@
+﻿using ExtensionMethodsExample;
+
+var myPizza = new Pizza();
+
+myPizza
+    .AddTopping<Cheese>()
+    .AddTopping<Salami>()
+    .AddTopping<Cheese>();
+
+myPizza.ShowToppings();
+      

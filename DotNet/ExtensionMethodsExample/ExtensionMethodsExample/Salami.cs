3066677a45690da0f456517877b0f5cd17088a9f
denidim <fs900220@gmail.com>
2026-03-18 20:48:03 +0000

    Add class Salami

---
+++ DotNet/ExtensionMethodsExample/ExtensionMethodsExample/Salami.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodsExample
+{
+    internal class Salami
+    {
+        public string Name => "Spicy Pepperoni";
+    }
+}
